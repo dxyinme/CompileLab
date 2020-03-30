bd518f222e9eb215a68f2e6acda69365c9afc0da
李奕鹏 <45395042+liyipeng123@users.noreply.github.com>
2020-03-30 20:40:41 +0800

    修改部分文件目录

---
+++ cmake-build-debug/CMakeFiles/cpp_demo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_demo.dir/main.cpp.obj"
+  "cpp_demo.exe"
+  "cpp_demo.exe.manifest"
+  "cpp_demo.pdb"
+  "libcpp_demo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
